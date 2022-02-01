1936dad0b715f4e780e0712b5a62461e911e5502
Gerhard Bräunlich <gerhard.braeunlich@id.ethz.ch>
2022-02-01 07:45:18 +0100

    fix pylint issues

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 #!/venv/bin/python
-#
-# Exampleapplication Helen
-# version 0
-# (c) 2021 T.Wisotzki
-#
 
+"""
+Exampleapplication Helen
+version 0
+(c) 2021 T.Wisotzki
+"""
+
+import sys
 import os.path
 import tkinter as tk
-import tkinter.messagebox as msg
 from docx import Document
 
 
 # global variable - defines where text-files are stored
-filepath = "texts/"
+TEXTS_PATH = "texts/"
 
 
 class DocumentCreator(tk.Tk):
+    """Main GUI"""
+
     def __init__(self, optionnames):
         super().__init__()
         self.title("Document creator")
         self.geometry("300x400")
 
         self.options = []
-        self.choosenOptions = []
+        self.choosen_options = []
 
         # instructions at top
         label = tk.Label(self, text="Select textblocks to include:")
@@ -30,31 +33,33 @@ class DocumentCreator(tk.Tk):
 
         # create checkboxes for every file
         for opname in optionnames:
-            newOption = tk.Checkbutton(self, text=opname)
-            newOption.bind("<Button-1>", self.add_option)
-            self.options.append(newOption)
+            new_option = tk.Checkbutton(self, text=opname)
+            new_option.bind("<Button-1>", self.add_option)
+            self.options.append(new_option)
         for option in self.options:
             option.pack()
 
         # generation Button
-        self.generateButton = tk.Button(
-            self, text="generate document", command=self.generateWord
+        self.generate_button = tk.Button(
+            self, text="generate document", command=self.generate_word
         )
-        self.generateButton.pack()
+        self.generate_button.pack()
 
-        self.exitButton = tk.Button(self, text="exit", command=self.exitApp)
-        self.exitButton.pack()
+        self.exit_button = tk.Button(self, text="exit", command=self.exit_app)
+        self.exit_button.pack()
 
-    # if text-part is de-/selected it is added/removed
     def add_option(self, event):
+        """if text-part is de-/selected it is added/removed"""
         task = event.widget.cget("text")
-        if task in self.choosenOptions:
-            self.choosenOptions.remove(task)
+        if task in self.choosen_options:
+            self.choosen_options.remove(task)
         else:
-            self.choosenOptions.append(task)
+            self.choosen_options.append(task)
 
-    def exitApp(self):
-        exit()
+    @staticmethod
+    def exit_app():
+        """Exit"""
+        sys.exit(0)
 
     #    # generation of combined textfile
     #    def generate(self):
@@ -65,28 +70,25 @@ class DocumentCreator(tk.Tk):
     #                with open(filepath + op + '.txt', 'r') as r:
     #                    f.write(r.read())
 
-    # generation of combined file as word
-    def generateWord(self):
+    def generate_word(self):
+        """generation of combined file as word"""
         document = Document()
         document.add_heading("Example Document template")
-        for op in self.choosenOptions:
-            with open(filepath + op + ".txt", "r") as r:
-                p = document.add_paragraph("")
-                p.add_run(r.read())
+        for option in self.choosen_options:
+            with open(TEXTS_PATH + option + ".txt", encoding="utf-8") as stream:
+                paragraph = document.add_paragraph("")
+                paragraph.add_run(stream.read())
         document.save("final.docx")
 
 
-# get all text-blocks in specified directory
-def getOptions():
-    if not os.path.isdir(filepath):
-        print("Error: path to text extracts not valid")
-        exit()
-    options = list()
-    for option in os.listdir(filepath):
-        options.append(option.split(".")[0])
-    return options
+def get_options():
+    """get all text-blocks in specified directory"""
+    if not os.path.isdir(TEXTS_PATH):
+        print("Error: path to text extracts not valid", file=sys.stderr)
+        sys.exit(1)
+    return [option.split(".")[0] for option in os.listdir(TEXTS_PATH)]
 
 
 if __name__ == "__main__":
-    documentcreator = DocumentCreator(getOptions())
+    documentcreator = DocumentCreator(get_options())
     documentcreator.mainloop()
